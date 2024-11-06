1c17da8382ee779d8b5936ba25894a89428cbd54
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2024-11-06 01:34:57 -0300

    chore: adjust default values

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,19 @@ import { env } from 'node:process';
 import { getProfiles, getUniqueProfiles } from './helpers/profile.js';
 import { randomize, writeResults } from './helpers/results.js';
 
-const { SPREADSHEET_ID = '', GID, COLUMN_INDEX = 0, WINNERS_NUMBER = 3 } = env;
+const {
+  SPREADSHEET_ID = '',
+  GID = '0',
+  COLUMN_INDEX = 0,
+  WINNERS_NUMBER = 3,
+} = env;
+
 const profiles = await getProfiles({
   spreadsheetId: SPREADSHEET_ID,
   gid: GID,
   columnIndex: Number(COLUMN_INDEX),
 });
+
 const uniqueProfiles = getUniqueProfiles(profiles);
 const randomProfiles = randomize(uniqueProfiles);
 
